a4afcc401e97167671aa8a2e7c727c07c659339a
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-01 19:44:21 +1300

    install eslint

--- main.ts
+++ main.ts
@@ -1,18 +1,26 @@
 
-var cli = process.argv[2]
+const cli = process.argv[2]
 
 main()
 
-function main() {
-    switch (cli) {
-        case 'encode':
-            encode()
-            break
-        case 'decode':
-            decode()
-            break
-        default:
-            console.error('Function requires encode or decode, try again')
-            process.exit(1)
-    }
+function main () {
+  switch (cli) {
+    case 'encode':
+      encode()
+      break
+    case 'decode':
+      decode()
+      break
+    default:
+      console.error('Function requires encode or decode, try again')
+      process.exit(1)
+  }
+}
+
+function encode () {
+  console.log('encoding')
+}
+
+function decode () {
+  console.log('decoding')
 }
