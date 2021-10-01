271a077b6aff724710627c587d8427efca37d1f2
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-01 19:35:22 +1300

    switch statement added to main function

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log('hello world')
+
+var cli = process.argv[2]
+
+main()
+
+function main() {
+    switch (cli) {
+        case 'encode':
+            encode()
+            break
+        case 'decode':
+            decode()
+            break
+        default:
+            console.error('Function requires encode or decode, try again')
+            process.exit(1)
+    }
+}
