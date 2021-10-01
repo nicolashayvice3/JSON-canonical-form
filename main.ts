b857e6ffe8b25c213e12b57d67172ebb6e68fffd
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-01 21:02:56 +1300

    processes choice of file

--- main.ts
+++ main.ts
@@ -1,20 +1,36 @@
+const fs = require('fs')
 
-const cli = process.argv[2]
+const file = process.argv[2]
+
+const action = process.argv[3]
 
 main()
 
 function main () {
-  switch (cli) {
+  const processedFile = processFile(file)
+  switch (action) {
     case 'encode':
-      encode()
+      encode(processedFile)
       break
     case 'decode':
-      decode()
+      decode(processedFile)
       break
     default:
       console.error('Function requires encode or decode, try again')
       process.exit(1)
   }
+}
+
+function processFile (file) {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      console.error('Need a adecuate file for JSON decoding or encoding')
+      process.exit(1)
+    }
+    const content = data
+
+    return content
+  })
 }
 
 function encode () {
