0de7ac926959bbffaaa1ea3aed783931720429b0
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-03 12:14:15 +1300

    maint.ts can be tested by test.sh

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
+#!/usr/bin/env node
+
 const fs = require('fs')
 
 const file = process.argv[2]
 
 main()
+console.log(file)
 
 function main () {
   const processedFile = processFile(file)
 
   const encoded = encode(processedFile)
-  console.log('encoded-main: ', encoded)
+  // console.log('encoded-main: ', encoded)
 
   const decoded = decode(encoded)
-  console.log('decoded-main: ', decoded)
 
-  return decoded // process.exit(0)
+  console.log(decoded); process.exit(0)
 }
 
 function processFile (file) {
@@ -22,13 +24,13 @@ function processFile (file) {
 }
 
 function encode (arg) {
-  const encoded = JSON.stringify(arg)
-  console.log('encoding: ', typeof (encoded), encoded)
+  const encoded = JSON.parse(arg)
+  // console.log('encoding: ', typeof (encoded), encoded)
   return encoded
 }
 
 function decode (arg) {
-  const decoded = JSON.parse(arg)
-  console.log('decoding: ', typeof (decoded), decoded)
+  const decoded = JSON.stringify(arg)
+  // console.log('decoding: ', typeof (decoded), decoded)
   return decoded
 }
--- test.sh
+++ test.sh
@@ -6,7 +6,6 @@ set -e
 
 # Verify processor executability.
 which "$1" >/dev/null || readlink -ve "$1" >/dev/null
-
 res=0
 for test in "`dirname "$0"`"/test/test-*.sh; do
 	$test "$@" || res=$?
