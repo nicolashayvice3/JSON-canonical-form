b036e1e13f29b82d0176bc02acd6f644ed45c4cb
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-10-03 19:46:12 +1300

    no-exponent trial

--- main.mjs
+++ main.mjs
@@ -13,8 +13,15 @@ function main () {
   const processedFile = processFile(file)
 
   const encoded = encode(processedFile)
-  // console.log('encoded-main: ', encoded)
 
+  for (let i = 0; i < encoded.length; i++) {
+    if (typeof encoded[i] === 'number') {
+      // console.log('short:', encoded[i])
+      const longNumber = toFixed(encoded[i])
+      encoded.splice(i, 1, longNumber)
+      // console.log('loong:', longNumber)
+    }
+  }
   const decoded = decode(encoded)
 
   console.log(decoded); process.exit(0)
@@ -35,4 +42,22 @@ function decode (arg) {
   const decoded = canonicalize(arg)
   // console.log('decoding: ', typeof (decoded), decoded)
   return decoded
+}
+
+function toFixed (x) {
+  if (Math.abs(x) < 1.0) {
+    const e = parseInt(x.toString().split('e-')[1])
+    if (e) {
+      x *= Math.pow(10, e - 1)
+      x = '0.' + (new Array(e)).join('0') + x.toString().substring(2)
+    }
+  } else {
+    let e = parseInt(x.toString().split('+')[1])
+    if (e > 20) {
+      e -= 20
+      x /= Math.pow(10, e)
+      x += (new Array(e + 1)).join('0')
+    }
+  }
+  return x
 }
